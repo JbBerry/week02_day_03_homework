aafbd8f7fdf84136f7371062c0397d4d2134b415
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-04-10 20:24:08 +0100

    all tasks completed, wrong syntax

--- advanced_enumeration/advanced_enumeration_start/upper_caser/upper_caser.js
+++ advanced_enumeration/advanced_enumeration_start/upper_caser/upper_caser.js
@@ -1,9 +1,14 @@
 class UpperCaser{
 
-  constructor (words) {}
+  constructor (words) {
+    this.words = words;
+  }
 
   toUpperCase() {
-
+    for (var i = 0; i < this.words.length; i++) {
+      this.words[i] = this.words[i].toUpperCase();
+    };
+    return this.words;
   }
 
 }
