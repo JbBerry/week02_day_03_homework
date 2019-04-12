195d33a4228196a7f0f715a8d795d7f14cc0ce20
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-04-12 10:16:34 +0100

    re-written some to higher order functions

--- advanced_enumeration/advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ advanced_enumeration/advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -8,21 +8,31 @@ class IsogramFinder{
    let letters = word.toLowerCase().split("").sort();
    return letters;
 // this code was in part obtained from "https://blog.anthonycomito.com/answer-the-anagram-interview-question-with-es6-c7621e43596d"
+  };
 
+//   isIsogram() {
+//     const letters = this.alphWord(this.word)
+// // check each element of the array, if it is the same as the next, then return false
+//     for (let i = 0; i < letters.length-1; i++) {
+//       if(letters[i] == letters[i+1]){
+//         return false;
+//         break;
+//       }
+//     }
+// // otherwise return true
+//     return true;
+//   };
 
- };
+  isIsogram(){
+    const letters = this.word.toLowerCase().split("").sort();
 
-  isIsogram() {
-    const letters = this.alphWord(this.word)
-// check each element of the array, if it is the same as the next, then return false
-    for (let i = 0; i < letters.length-1; i++) {
-      if(letters[i] == letters[i+1]){
-        return false;
-        break;
-      }
-    }
-// otherwise return true
-    return true;
+    unique(letters){
+      array.forEach(function(character, index, letters){
+        if (character == array[index+1]){
+          return false;
+        }
+      },0);
+    };
   };
 }
 
--- advanced_enumeration/advanced_enumeration_start/upper_caser/upper_caser.js
+++ advanced_enumeration/advanced_enumeration_start/upper_caser/upper_caser.js
@@ -4,13 +4,19 @@ class UpperCaser{
     this.words = words;
   }
 
-  toUpperCase() {
-    for (var i = 0; i < this.words.length; i++) {
-      this.words[i] = this.words[i].toUpperCase();
-    };
-    return this.words;
-  }
+  // toUpperCase() {
+  //   for (var i = 0; i < this.words.length; i++) {
+  //     this.words[i] = this.words[i].toUpperCase();
+  //   };
+  //   return this.words;
+  // }
 
+
+  toUpperCase(){
+    return = this.words.map(function(word){
+      return word.toUpperCase();
+    });
+  };
 }
 
 module.exports = UpperCaser;
